b46777ce73426cc1dd218b249b099daf2a73d388
Adrian111cool <adriancruzmadrid@gmail.com>
2021-08-14 10:44:37 -0500

    Vectors

---
+++ Assets/Scripts/Vectors.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vectors : MonoBehaviour
+{
+    public float speed;
+    int localBase = 1;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.localScale = Vector2.one* (localBase + speed);
+            speed += .1f;
+        }
+        transform.Rotate(Vector3.forward, speed);
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.localScale = Vector2.one * (localBase - speed);
+            speed -= .1f;
+            transform.Rotate(Vector3.forward, -speed);
+        }
+
+    }
+}
