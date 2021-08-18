a84c04597c89afefb3b58ee3edb62d6c51d60bee
Adrian111cool <adriancruzmadrid@gmail.com>
2021-08-17 20:28:44 -0500

    PlayerController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rigidBody;
+    public int direction;
+    public float moveSpeed;
+    public Vector2 inputValue;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        MovementInput();
+    }
+
+    private void FixedUpdate()
+    {
+        MovementAction();
+    }
+
+    void Movement_1()
+    {
+        if (Input.GetKey(KeyCode.A))
+            direction = -1;
+        else if (Input.GetKey(KeyCode.D))
+            direction = 1;
+        else if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+            direction = 0;
+
+        rigidBody.velocity = new Vector2(direction * moveSpeed, rigidBody.velocity.y);
+
+    }
+
+    void MovementInput()
+    {
+        inputValue = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, rigidBody.velocity.y);
+    }
+
+    void MovementAction()
+    {
+        rigidBody.velocity = inputValue;
+    }
+}
