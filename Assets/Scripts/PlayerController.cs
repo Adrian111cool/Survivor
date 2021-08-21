c5e17f74614ae4642747c135930b2ce02f9b362e
Adrian111cool <adriancruzmadrid@gmail.com>
2021-08-21 10:47:56 -0500

    Player Controller - Salto Simple

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,8 +7,15 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rigidBody; // Es la variable que luego de enlazarse con el componente
                                   // Maneja las físicas de nuestro player.
+
+    [Header("Movement Properties")]
     public int direction; // Es la dirección a la que se está moviendo nuestro player
     public float moveSpeed; // Es la velocidad a la que se mueve nuestro player
+
+    [Header("Jump Properties")]
+    public float jumpForce; // La potencia del salto
+    public bool jumpInputOn; // He presionado el botón de salto?
+
     public Vector2 inputValue; // Es para ver el input que estamos haciendo cuando presionamos los botones
                                // Registrados en Input.GetAxis o Input.GetAxisRaw
 
@@ -24,14 +31,26 @@ public class PlayerController : MonoBehaviour
 
         // Llamar a la función MovementInput, donde mando el input a mi variable inputValue
         MovementInput();
+
+        //Si se presiona el botón space, se activa la variable jumpInputOn
+        if (Input.GetKeyDown(KeyCode.Space))
+            jumpInputOn = true;
     }
 
     private void FixedUpdate()
     {
         // Mover a mi player según el input registrado en la función MovementInput.
         MovementAction();
+
+        //Si la variable JumpInputOn está activa (es true), entonces has el salto, y apaga la variable.
+        if (jumpInputOn)
+        {
+            Jump();
+            jumpInputOn = false;
+        }
     }
 
+    #region Movement
     void Movement_1()
     {
         if (Input.GetKey(KeyCode.A))
@@ -44,7 +63,6 @@ public class PlayerController : MonoBehaviour
         rigidBody.velocity = new Vector2(direction * moveSpeed, rigidBody.velocity.y);
 
     }
-
 
     void MovementInput()
     {
@@ -80,6 +98,18 @@ public class PlayerController : MonoBehaviour
         //Aquí se pasan los valores que registramos en MovementInput, y se mandan al rigidbody, para que se mueva.
         rigidBody.velocity = inputValue;
     }
+
+    #endregion
+
+    #region Jump
+
+    void Jump()
+    {
+        //Aquí se le da una fuerza en el eje y o en el eje vertical.
+        rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
+    }
+
+    #endregion
 
     void FlipSprite()
     {
